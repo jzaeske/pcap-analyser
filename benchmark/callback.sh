e46d1505793c55e701eefb995bc0bea6b7577bdf
Jan Zaeske <j.zaeske@addmore.de>
2017-03-02 12:13:05 +0100

    OUTPATH angepasst

--- benchmark/callback.sh
+++ benchmark/callback.sh
@@ -5,7 +5,7 @@ OUTPATH=$1
 go test -timeout 60m -bench ^BenchmarkCallback.*_1000$ -benchmem pipeline/callback_test.go \
     | grep -e "^Benchmark" \
     | sed "s/[_-]/ /g" \
-    | tee -a ~/Dokumente/studium/masterarbeit/thesis/reports/benchmark_raw.txt \
+    | tee -a $OUTPATH/benchmark_raw.txt \
     | awk '{print $2,$3,$6}' \
     | sort -n \
     >> $OUTPATH/benchmark_callback_filters.csv
@@ -13,7 +13,7 @@ go test -timeout 60m -bench ^BenchmarkCallback.*_1000$ -benchmem pipeline/callba
 go test -timeout 60m -bench ^BenchmarkCallback_100_.*$ -benchmem pipeline/callback_test.go \
     | grep -e "^Benchmark" \
     | sed "s/[_-]/ /g" \
-    | tee -a ~/Dokumente/studium/masterarbeit/thesis/reports/benchmark_raw.txt \
+    | tee -a $OUTPATH/benchmark_raw.txt \
     | awk '{print $3,$2,$6}' \
     | sort -n \
     >> $OUTPATH/benchmark_callback_packets.csv
--- benchmark/channel.sh
+++ benchmark/channel.sh
@@ -5,7 +5,7 @@ OUTPATH=$1
 go test -timeout 60m -bench ^BenchmarkChannel.*_1000$ -benchmem pipeline/channel_test.go \
     | grep -e "^Benchmark" \
     | sed "s/[_-]/ /g" \
-    | tee -a ~/Dokumente/studium/masterarbeit/thesis/reports/benchmark_raw.txt \
+    | tee -a $OUTPATH/benchmark_raw.txt \
     | awk '{print $2,$3,$6}' \
     | sort -n \
     >> $OUTPATH/benchmark_channel_filters.csv
@@ -13,7 +13,7 @@ go test -timeout 60m -bench ^BenchmarkChannel.*_1000$ -benchmem pipeline/channel
 go test -timeout 60m -bench ^BenchmarkChannel_100_.*$ -benchmem pipeline/channel_test.go \
     | grep -e "^Benchmark" \
     | sed "s/[_-]/ /g" \
-    | tee -a ~/Dokumente/studium/masterarbeit/thesis/reports/benchmark_raw.txt \
+    | tee -a $OUTPATH/benchmark_raw.txt \
     | awk '{print $3,$2,$6}' \
     | sort -n \
     >> $OUTPATH/benchmark_channel_packets.csv
